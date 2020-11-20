7e59c9f8d0b710275a58cbea6abcb989f902e3dc
ChocolaterToba <Toba.ru@mail.ru>
2020-11-20 13:33:23 +0300

    Revert "Http response rk 2"

--- http_server/include/HttpRequest.hpp
+++ http_server/include/HttpRequest.hpp
@@ -1,27 +1,6 @@
 #pragma once
 
-// That header just provide me the mocks to write my code properly. However, all these features are implemented by my team in their
-// branches.
-
-#include <string>
-
-typedef enum {
-    UNKNOWN = -1,
-    GET,
-    POST,
-    OPTIONS,
-    HEAD,
-    PUT,
-    PATCH,
-    DELETE,
-    CONNECT,
-} RequestMethod;
 
 class HttpRequest {
-public:
-    HttpRequest(std::vector<char>) {}
-
-    virtual std::string GetURL() const {return "index.html";}
-    virtual RequestMethod GetRequestMethod() const {return GET;}
+    
 };
-
